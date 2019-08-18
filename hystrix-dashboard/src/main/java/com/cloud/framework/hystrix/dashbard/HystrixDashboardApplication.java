71b29294b67b68d31c058ed97eed04adccb98150
tengyd <tengyd@hd100.com>
2019-08-18 15:20:46 +0800

    整合zuul

--- hystrix-dashboard/src/main/java/com/cloud/framework/hystrix/dashbard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/cloud/framework/hystrix/dashbard/HystrixDashboardApplication.java
@@ -1,18 +1,16 @@
-package com.cloud.framework.user;
+package com.cloud.framework.hystrix.dashbard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
-@EnableHystrix
+@EnableHystrixDashboard
 @EnableDiscoveryClient
 @SpringBootApplication
-public class UserServiceApplication {
-
+public class HystrixDashboardApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(HystrixDashboardApplication.class, args);
     }
-
 }
--- order-service/src/main/java/com/cloud/framework/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/cloud/framework/order/OrderServiceApplication.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableHystrixDashboard
 @EnableHystrix
 @EnableFeignClients
 @EnableDiscoveryClient
